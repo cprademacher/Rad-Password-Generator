53382e94785dc9f50ff2b4c03e73a332dab74071
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-06-30 10:54:32 -0500

    Add javascript code that prompts and checks for a password beetween 8 and 128 characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+function generatePassword() {
+	var length = prompt("Type desired lenght of password between 8 and 128 characters.").length;
+	var response;
+
+	if (length == 0) {
+		response = 'No answer given.';
+	} else if (length < 8) {
+		response = 'Password is too short.';
+	} else if (length > 128) {
+		response = 'Password is too long.';
+	} else {
+		response = 'Password is just right.';
+	}
+	alert(response);
+
+}
+
 // Write password to the #password input
 function writePassword() {
 	var password = generatePassword();
 	var passwordText = document.querySelector('#password');
 
 	passwordText.value = password;
+
+	
 }
 
 // Add event listener to generate button
