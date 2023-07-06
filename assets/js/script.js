ffc5e4b174263f576a308b6470a9a4a72ed017e4
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-05 21:27:48 -0500

    changed js file and added functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
-// Below asks for desired length of password, makes sure 
-// it is a number, and stores it as 'response', as long as it is between 
-// 8 and 128 characters.
-function askForLength() {
-	var answer = prompt("Type desired lenght of password between 8 and 128 characters.");
-	// var answer = parseInt(ask);
-	var response;
-	var correct;
 
-		if (Number(answer) == 0) {
-			correct = 'No answer given.';
-			alert(correct);
-		} else if (Number(answer) < 8) {
-			correct = 'Length must be greater or equal to 8.';
-			alert(correct);
-		} else if (Number(answer) > 128) {
-			correct = 'Length must be no more than 128';
-			alert(correct);
-		} else if (Number(answer) >= 8 && Number(answer) <= 128) {
-			response = answer;
-		} else {
-		correct = "You must enter a number";
-		alert(correct);
-	}
-	console.log(response);
-};
+function generatePassword() {
+	// The variables below give us all the possible values of each type of character the user could choose from
+	var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+	var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var numberCharacters = '1234567980';
+	var specialCharacters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
 
-// Below is a function that will confirm whether or not the user wants to use
-// different types of characters in their password.  At least one of these has to
-// be used, but any or all can be included.
+	// Prompt for the user's desired password length, between 8 and 128 characters
+	var lengthAnswer = prompt("Type desired lenght of password between 8 and 128 characters.");
 
-function askForType() {
-	var lowerCase = confirm('Do you want to use lower case letters in your password?');
-	var upperCase = confirm('Do you want to use upper case letters in your password?');
-	var numValue = confirm('Do you want to use numbers in your password?');
-	var specialCharacters = confirm('Do you want to use special characters in your password?');
-
-	if(lowerCase || upperCase || numValue || specialCharacters) {
-		if(lowerCase) {
-			console.log('Lower Case Letters have been selected.');
+	// Check that the user's input was within the allowed range, if not, alert them that they must submit a number within the givin range
+	if (lengthAnswer < 8 || lengthAnswer > 128) {
+			alert('Invalid entry: must be between 8 and 128');
+			return; //since the entry is invalid, we shall return without collecting anything
 		} 
 
-		if(upperCase) {
-			console.log('Upper Case Letters have been selected.');
-		}
+	// Prompt the user to confirm whether or not they want to use each kind of character.
+	var includeLowerCase = confirm('Do you want to use lower case letters in your password?');
+	var includeUpperCase = confirm('Do you want to use upper case letters in your password?');
+	var includeNumValue = confirm('Do you want to use numbers in your password?');
+	var includeSpecialCharacters = confirm('Do you want to use special characters in your password?');
 
-		if(numValue) {
-			console.log('Numbers have been selected.');
-		}
-
-		if(specialCharacters) {
-			console.log('Special Characters have been selected.');
-		}
-	} else {
-		alert('You must select at least one type of character to use in your password.')
+	// Make sure the user selected at least one kind of character to use, if not, alert them that they must use at least one
+	if(!includeLowerCase && !includeUpperCase && !includeNumValue && !includeSpecialCharacters) {
+		alert('You must select at least one type of character to use in your password.');
+		return;
 	}
-}
 
+	// Generate a password based on the selected criteria
+	var password = '';
+	var availableCharacters = '';
 
+	// Check if lower case letters are selected, if so, add them to available characters
+	if(includeLowerCase) {
+		availableCharacters += lowerCaseCharacters;
+	}
 
+	// Check if upper case letters are selected, if so, add them to available characters
+	if(includeUpperCase) {
+		availableCharacters += upperCaseCharacters;
+	}
 
-// The function below calls the functions we have created above.
-function generatePassword() {
-	askForLength();
-	askForType();
-}
+	// Check if numbers are selected, if so, add them to available characters
+	if(includeNumValue) {
+		availableCharacters += numberCharacters;
+	}
 
-// Write password to the #password input
-function writePassword() {
-	var password = generatePassword();
+	// Check if special characters are selected, if so, add them to available characters
+	if(includeSpecialCharacters) {
+		availableCharacters += specialCharacters;
+	}
+
+	// Loop through the length that the user input in the first prompt
+	for(var i = 0; i < lengthAnswer; i++) {
+		// Creates a random number based on the the amount of characters the user input and saves it to the variable randomI
+		var randomIndex = Math.floor(Math.random() * availableCharacters.length);
+		// Adds random characters from the available characters list to the password for as many characters as the user input (saved in randomI)
+		password += availableCharacters.charAt(randomIndex);
+	}
+	
+	// Select the element with the ID of 'password' and set it equal to the variable passwordText
 	var passwordText = document.querySelector('#password');
 
+	// Set the variable passwordText's value equal to that of the generated password.
 	passwordText.value = password;
-
-	
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+// Add event listener to generate button that will call the generatePassword function
+generateBtn.addEventListener('click', generatePassword);
